f1d4bc09861312e1f492ac6a5889cbbc4fbfb40e
kazijamal <jamalkazi02@gmail.com>
2022-10-29 11:20:33 -0400

    Leftover code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import * as Y from 'yjs';
 
 import TextDocument from './components/TextDocument';
 import ConnectForm from './components/ConnectForm';
 
 function App() {
     const [docOpen, setDocOpen] = useState(false);
+    const ydoc = new Y.Doc();
 
     return docOpen ? <TextDocument /> : <ConnectForm setDocOpen={setDocOpen} />;
 }
--- server/App.js
+++ server/App.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const cors = require('cors');
+
+const Y = require('yjs');
+const LeveldbPersistence = require('y-leveldb').LeveldbPersistence;
+const persistence = new LeveldbPersistence('./leveldb');
 
 app.use(cors());
 
 app.use('/library', express.static('library'));
 
-app.get('/api/connect/:id', (req, res) => {
+app.get('/api/connect/:id', async (req, res) => {
     const { id } = req.params;
-    console.log(id);
+    const yDoc = await persistence.getYDoc(id);
     res.writeHead(200, {
         Connection: 'keep-alive',
         'Content-Type': 'text/event-stream',
@@ -21,6 +24,20 @@ app.get('/api/connect/:id', (req, res) => {
         res.write(`data: ${JSON.stringify({ hasUnread: true })}`);
         res.write('\n\n');
     }, 5000);
+});
+
+app.get('/', async (req, res) => {
+    const ydoc = new Y.Doc();
+    ydoc.getArray('arr').insert(0, [1, 2, 3]);
+    ydoc.getArray('arr').toArray(); // => [1, 2, 3]
+    persistence.storeUpdate('my-doc', Y.encodeStateAsUpdate(ydoc));
+    const gotdoc1 = await persistence.getYDoc('no-exist');
+    console.log(gotdoc1);
+    console.log(gotdoc1.store.clients.length);
+    const gotdoc2 = await persistence.getYDoc('my-doc');
+    console.log(gotdoc2);
+    console.log(gotdoc2.store.clients.length);
+    res.send('egsiognesoignsjk');
 });
 
 app.listen(5001, () => {
